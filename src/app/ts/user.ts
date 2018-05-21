a073c032a4fa12b3f69a179fc05ed4334eb0b2f3
dbukatyy <dbukatyy@mail.ru>
2018-05-21 15:02:31 +0300

    fix lint error

--- src/app/ts/user.ts
+++ src/app/ts/user.ts
@@ -1,23 +1,22 @@
-
 export class User {
-	id: number;
-	name: string;
-	email: string;
-	address: {
-	    street: string;
-	    suite: string;
-	    city: string;
-	    zipcode: number;
-	    geo: {
-	    	lat: number;
-	    	lng: number;
-	    };
-	};
-	phone: number;
-	website: string;
-	company: {
-		name: string;
-		catchPhrase: string;
-		bs: string;
-	};
-};
+  id: number;
+  name: string;
+  email: string;
+  address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: number;
+      geo: {
+        lat: number;
+        lng: number;
+      };
+  };
+  phone: number;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
--- src/app/ts/users-list.component.ts
+++ src/app/ts/users-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from './user'
+import { User } from './user';
 
 @Component({
   selector: 'app-users-list',
@@ -15,33 +15,33 @@ export class UsersListComponent implements OnInit {
 
   openModal: boolean;
 
-  dataLoaded: boolean = false;
+  dataLoaded = false;
 
   constructor() { }
 
   ngOnInit() {
 
-  	this.openModal = localStorage.getItem('openModal') === 'true' ? true : false;
-  	this.selectedUser = JSON.parse(localStorage.getItem('selectedUser'));
+    this.openModal = localStorage.getItem('openModal') === 'true' ? true : false;
+    this.selectedUser = JSON.parse(localStorage.getItem('selectedUser'));
 
-  	fetch('http://jsonplaceholder.typicode.com/users')
-  		.then(response => response.json())
-  		.then(persons => {
-    		this.users = persons;
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(persons => {
+        this.users = persons;
         this.dataLoaded = true;
-  		})
+      });
   }
 
   onSelect(user: User): void {
-  	this.selectedUser = user;
-  	this.openModal = true;
-  	localStorage.setItem('openModal', 'true');
-  	localStorage.setItem('selectedUser', JSON.stringify(this.selectedUser));
+    this.selectedUser = user;
+    this.openModal = true;
+    localStorage.setItem('openModal', 'true');
+    localStorage.setItem('selectedUser', JSON.stringify(this.selectedUser));
   }
 
   onModalOff(e): void {
-  	this.openModal = false;
-  	localStorage.setItem('openModal', 'false');
+    this.openModal = false;
+    localStorage.setItem('openModal', 'false');
   }
 
 }
