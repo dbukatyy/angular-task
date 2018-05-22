2df5f6162610cc7c181b2d4fe1acfcd07f8055af
dbukatyy <dbukatyy@mail.ru>
2018-05-22 12:25:31 +0300

    fix test

--- src/app/ts/users-list.component.spec.ts
+++ src/app/ts/users-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersListComponent } from './users-list.component';
+import { ModalComponent } from './modal.component';
 
 describe('UsersListComponent', () => {
   let component: UsersListComponent;
@@ -8,7 +9,7 @@ describe('UsersListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersListComponent ]
+      declarations: [ UsersListComponent, ModalComponent ]
     })
     .compileComponents();
   }));
